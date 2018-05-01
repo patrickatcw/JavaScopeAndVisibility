11f518ba5d0a6b73d5825297a7d00a977c7db043
patrickatcw <pj8392@yahoo.com>
2018-05-01 11:26:51 -0400

    initial program structure, updated gitignore, initial code in scopecheck from JavaScope program

---
+++ src/com/me/ScopeCheck.java
@@ -0,0 +1,57 @@
+package com.me;
+//initial code from JavaScope program
+//class
+public class ScopeCheck {
+
+    //variables
+    public int publicVar = 0;
+    private int privateVar = 1;
+
+    //constructor with no params
+    public ScopeCheck() {
+        System.out.println("ScopeCheck created, publicVar = " + publicVar +
+                ": privateVar = " + privateVar);
+    }
+
+    //getter just for private var
+    public int getPrivateVar() {
+        return privateVar;
+
+        //go to main
+    }
+
+    //method
+    public void timesTwo() {
+        int privateVar = 2;
+        for (int i = 0; i < 10; i++) {
+            /*System.out.println(i + " times two is " +
+                    i * privateVar);*/   //*if i wanted to use private int privateVar = 1; this.privateVar
+
+            //back to main
+        }
+
+    }
+
+    //new class, inside class
+    public class InnerClass {
+
+        //variable
+        public int privateVar = 3; //if this was not here, program would find private int privateVar = 1;
+
+        //constructor
+        public InnerClass() {
+            System.out.println("InnereClass created, privateVar is " + privateVar);
+        }
+
+        //method from above, now in Innerclass, comment out sout in method above
+        public void timesTwo() {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i + " times two is " +
+                        i * privateVar);
+
+                //back to main to create an instance for Innerclass
+            }
+        }
+    }
+
+}
